ddf7bc30c967750d8184c59f44a49020426a8a92
CaioAlves <caioalves.fmc@gmail.com>
2024-02-21 14:52:24 -0300

    versão 1.1

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -88,7 +88,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-l8gc.onrender.com")
     websocket.onmessage = processMessage
 
 
